2a5c96176b7623f071a236519b9436022abd2162
chaihahaha <836275709@qq.com>
2017-12-26 17:50:31 +0800

    Update and rename SingleStringPractice.py to SingleStringPracticeForWindows.py

--- SingleStringPractice.py
+++
@@ -1,7 +0,0 @@
-import random
-import time
-import win32com.client
-speaker = win32com.client.Dispatch("SAPI.SpVoice")
-while True:
-    speaker.Speak(chr(random.randint(1,7)+64))
-    time.sleep(5)
