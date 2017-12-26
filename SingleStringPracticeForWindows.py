c5ebbe63e832707a2fd19f0825125c797e3f7200
chaihahaha <836275709@qq.com>
2017-12-26 18:08:11 +0800

    Update SingleStringPracticeForWindows.py

--- SingleStringPracticeForWindows.py
+++ SingleStringPracticeForWindows.py
@@ -4,10 +4,10 @@ import win32com.client
 speaker = win32com.client.Dispatch("SAPI.SpVoice")
 char1 = chr(random.randint(1,7)+64)
 char2 = chr(random.randint(1,7)+64)
+dt=float(input("Please enter the time interval:"))
 while True:
     while char1==char2:
         char2 = chr(random.randint(1,7)+64)
     speaker.Speak(char1)
-    print(char1)
     char1=char2
-    time.sleep(3)
+    time.sleep(dt)
