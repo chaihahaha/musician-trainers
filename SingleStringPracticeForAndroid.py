eed4936026e6846d5ef288c9b910e9ad17a64566
chaihahaha <836275709@qq.com>
2017-12-25 23:07:27 -0600

    Update SingleStringPracticeForAndroid.py

--- SingleStringPracticeForAndroid.py
+++ SingleStringPracticeForAndroid.py
@@ -1,13 +1,22 @@
+"""Speak the note name."""
+  
 import sl4a
 import time
 import random
+char1 = chr(int((random.random()*10000000000)**2)%7+1+64)
+char2 = chr(int((random.random()*10000000000)**2)%7+1+64)
+
 while True:
     time.sleep(4)
     droid = sl4a.Android()
-    char = chr(int((random.random()*10000000000)**2)%7+1+64)
-    print(char)
-    if char=='B':
-        char='Be'
-    if char=='E':
-        char='ea'
-    droid.ttsSpeak(char)
+    while char1==char2:
+        char2 = chr(int((random.random()*10000000000)**2)%7+1+64)
+    print(char1)
+    if char1=='B':
+        char1='Be'
+    if char1=='E':
+        char1='ea'
+    droid.ttsSpeak(char1)
+    char1=char2
+    
+
