f0a7ea0a26d25c3096325034cf42ef36b2ab7f62
chaihahaha <836275709@qq.com>
2017-12-27 08:42:42 -0500

    12.27

---
+++ SingleStringPracticeForLinux.py
@@ -0,0 +1,14 @@
+import random
+import time
+import pyttsx3
+engine = pyttsx3.init()
+char1 = chr(random.randint(1,7)+64)
+char2 = chr(random.randint(1,7)+64)
+dt=float(input("Please enter the time interval:"))
+while True:
+    while char1==char2:
+        char2 = chr(random.randint(1,7)+64)
+    engine.say(char1)
+    engine.runAndWait()
+    char1=char2
+    time.sleep(dt)
