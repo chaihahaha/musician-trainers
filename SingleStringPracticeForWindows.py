7830ab4c8965784d82010e2d2a49e58684714a5e
chaihahaha <836275709@qq.com>
2018-01-12 23:26:52 +0800

    Update SingleStringPracticeForWindows.py

--- SingleStringPracticeForWindows.py
+++ SingleStringPracticeForWindows.py
@@ -8,6 +8,10 @@ dt=float(input("Please enter the time interval:"))
 while True:
     while char1==char2:
         char2 = chr(random.randint(1,7)+64)
-    speaker.Speak(char1)
+    if char1=='B':
+        speaker.Speak('BB')
+    else:
+        speaker.Speak(char1)
+    print(char1)
     char1=char2
     time.sleep(dt)
