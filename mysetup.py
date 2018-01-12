0816e30a0d0fcddd41bab4f99b75904a14cda8d0
chaihahaha <836275709@qq.com>
2018-01-12 23:36:52 +0800

    Update mysetup.py

--- mysetup.py
+++ mysetup.py
@@ -1,3 +1,4 @@
+#This script is created to help you build binary executables with the package 'py2exe'. You can build by executing 'python mysetup.py py2exe'.
 from distutils.core import setup
 import py2exe
 setup(console=["SingleStringPractice.py"])
